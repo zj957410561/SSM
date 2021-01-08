a17a87939b5150988b8ec48119ca6f34dfc5f94a
zj957410561 <957410561@qq.com>
2021-01-08 15:49:58 +0800

    哈哈

--- src/main/java/com/cn/zj/realm/UserRealm.java
+++ src/main/java/com/cn/zj/realm/UserRealm.java
@@ -1,9 +1,11 @@
 package com.cn.zj.realm;
 
+import com.cn.zj.service.StudentService;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.apache.shiro.realm.Realm;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * TODO
@@ -13,6 +15,14 @@ import org.apache.shiro.realm.Realm;
  * @date 2021/1/7 17:27
  */
 public class UserRealm implements Realm {
+
+    StudentService studentService;
+
+    @Autowired
+    public void setStudentService(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @Override
     public String getName() {
         return null;
